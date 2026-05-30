28e393069d761c3f0bac208dc4f79db8cb0e2bbb
radhakrishnaa2995-png <radhakrishnaa2995@gmail.com>
2026-05-30 15:26:13 +0530

    Update test_sampling.py

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -1,1 +1,6 @@
+from src.dot_to_dot import sampling
 
+def test_sample_points():
+    contour = [(0,0), (10,0), (10,10), (0,10)]
+    points = sampling.sample_points(contour, 4)
+    assert len(points) == 4
