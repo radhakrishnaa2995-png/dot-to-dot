ea5c4bcc132915f1d6c4b0ae3093df5439e97515
radhakrishnaa2995-png <radhakrishnaa2995@gmail.com>
2026-05-30 15:26:45 +0530

    Update test_pdf.py

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,1 +1,16 @@
+from src.dot_to_dot import pdf_builder
 
+def test_create_pdf():
+    # create dummy images
+    from PIL import Image
+    img1 = Image.new('RGB', (595, 842), 'white')  # A4 size
+    img1_path = 'test_page1.png'
+    img1.save(img1_path)
+    output_pdf = 'test_output.pdf'
+    pdf_builder.create_pdf([img1_path], output_pdf)
+    import os
+    assert os.path.exists(output_pdf)
+    # cleanup
+    import os
+    os.remove(img1_path)
+    os.remove(output_pdf)
