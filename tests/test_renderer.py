1b5a0e1ed205262fbc0809b5ff0d41913d892b0c
radhakrishnaa2995-png <radhakrishnaa2995@gmail.com>
2026-05-30 15:26:28 +0530

    Update test_renderer.py

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -1,1 +1,7 @@
+from src.dot_to_dot import renderer
 
+def test_render_dots():
+    dots = [(10,10), (50,50), (90,10)]
+    img = renderer.render_dots(dots)
+    assert img.size[0] > 0 and img.size[1] > 0
+    img.save("test_output.png")
